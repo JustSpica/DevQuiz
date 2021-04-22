a4a0e266428796de9af04898b6e35c6810b03d09
Guilherme Luiz <guilhermespicaluiz@gmail.com>
2021-04-22 16:02:37 -0300

    create firebase db

--- app/src/main/java/com/example/devquiz/SignInActivity.kt
+++ app/src/main/java/com/example/devquiz/SignInActivity.kt
@@ -10,15 +10,6 @@ import org.jetbrains.anko.startActivity
 class SignInActivity : AppCompatActivity() {
 
     private val buttonSignUp by lazy { findViewById<Button>(R.id.buttonSignUp) }
-    // [START declare_database_ref]
-    private lateinit var database: DatabaseReference
-    // [END declare_database_ref]
-
-    fun initializeDbRef() {
-        // [START initialize_database_ref]
-        database = Firebase.database.reference
-        // [END initialize_database_ref]
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
