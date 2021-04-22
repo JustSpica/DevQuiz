cab152be54206f20f9754cb9a8de882db577f9bf
lucas <lucasjfvieira@gmail.com>
2021-04-22 14:59:20 -0300

    Firebase database created... no codes includes, just the implementations

--- app/src/main/java/com/example/devquiz/SignInActivity.kt
+++ app/src/main/java/com/example/devquiz/SignInActivity.kt
@@ -3,11 +3,22 @@ package com.example.devquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
 import org.jetbrains.anko.startActivity
 
 class SignInActivity : AppCompatActivity() {
 
     private val buttonSignUp by lazy { findViewById<Button>(R.id.buttonSignUp) }
+    // [START declare_database_ref]
+    private lateinit var database: DatabaseReference
+    // [END declare_database_ref]
+
+    fun initializeDbRef() {
+        // [START initialize_database_ref]
+        database = Firebase.database.reference
+        // [END initialize_database_ref]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +27,6 @@ class SignInActivity : AppCompatActivity() {
         buttonSignUp.setOnClickListener {
             startActivity<SignUpActivity>()
         }
+
     }
 }
