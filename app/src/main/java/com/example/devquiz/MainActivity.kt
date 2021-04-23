30aabc32f35a128c1012f1800de9b3fd4d8daf29
Guilherme Luiz <guilhermespicaluiz@gmail.com>
2021-04-23 02:15:29 -0300

    update layout

--- app/src/main/java/com/example/devquiz/MainActivity.kt
+++ app/src/main/java/com/example/devquiz/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.devquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private val borderButtonStart by lazy { findViewById<ImageView>(R.id.borderButtonStart) }
+    private val buttonStart by lazy { findViewById<Button>(R.id.buttonStart) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonStart.setOnClickListener {
+            spinAnimation()
+        }
+    }
+
+    private fun spinAnimation() {
+        val spinAnimation: Animation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.spin_animation_button_start
+        )
+
+        borderButtonStart.startAnimation(spinAnimation)
     }
 }
