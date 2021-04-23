9bdf53bfaefd88c516184ef935c5620a4de0d672
Guilherme Luiz <guilhermespicaluiz@gmail.com>
2021-04-23 02:37:17 -0300

    bug fixes on the project screens

--- app/src/main/java/com/example/devquiz/SignUpActivity.kt
+++ app/src/main/java/com/example/devquiz/SignUpActivity.kt
@@ -2,10 +2,20 @@ package com.example.devquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import org.jetbrains.anko.startActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val buttonSignUp by lazy { findViewById<Button>(R.id.buttonActivitySignUpScreen) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        buttonSignUp.setOnClickListener {
+            startActivity<MainActivity>()
+        }
+
     }
 }
