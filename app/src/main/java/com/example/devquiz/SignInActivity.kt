15827092c11d29cc9da3f2b9b70ed6d3908d916d
lucas <lucasjfvieira@gmail.com>
2021-04-24 00:24:03 -0300

    database link code created but is not working very well... x(

--- app/src/main/java/com/example/devquiz/SignInActivity.kt
+++ app/src/main/java/com/example/devquiz/SignInActivity.kt
@@ -3,21 +3,52 @@ package com.example.devquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.ktx.Firebase
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class SignInActivity : AppCompatActivity() {
 
+    //Referenciação dos elementos da tela
     private val buttonSignUp by lazy { findViewById<Button>(R.id.buttonSignUp) }
+    private val buttonLogIn by lazy { findViewById<Button>(R.id.buttonActivitySignInScreen) }
+    private val email by lazy { findViewById<EditText>(R.id.emailInputSignIn) }
+    private val password by lazy { findViewById<EditText>(R.id.passwordInputSignIn) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        //faz o botao "entrar" logar o usuario e ir para o quiz
+        buttonLogIn.setOnClickListener {
+            loginUser()
+        }
+        //faz o botao "cadastre-se" ir parea a tela de cadsastro
         buttonSignUp.setOnClickListener {
             startActivity<SignUpActivity>()
         }
+    }
 
+    //método para fazer o login do usuário e verificar se os campos estão preenchidos
+    private fun loginUser() {
+        val email: String = email.text.toString()
+        val password: String = password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            toast("Preencha todos os campos!!!")
+        } else {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener  /*caso dê certo, ele manda a toast e vai pra tela de start*/{
+                    if (it.isSuccessful) {
+                        toast("Cadastro realizado com SUCESSO!!!")
+                        buttonSignUp.setOnClickListener {
+                            startActivity<MainActivity>()
+                        }
+                    }
+                }.addOnFailureListener /*caso n logue o usuário, gera uma toast de erro*/{
+                        toast("Erro ao tentar logar o usuário x(")
+                }
+        }
     }
 }
--- app/src/main/java/com/example/devquiz/SignUpActivity.kt
+++ app/src/main/java/com/example/devquiz/SignUpActivity.kt
@@ -2,10 +2,55 @@ package com.example.devquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class SignUpActivity : AppCompatActivity() {
+
+
+    private val buttonSingUp by lazy { findViewById<Button>(R.id.buttonActivitySignUpScreen) }
+    private val userNameEditText by lazy { findViewById<EditText>(R.id.userNameInputSignUp) }
+    private val emailEditText by lazy { findViewById<EditText>(R.id.emailInputSignUp) }
+    private val passwordEditText by lazy { findViewById<EditText>(R.id.passwordlInputSignUp) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        //setando o botão cadastrar para executar os métodos de cadastro do bd
+        buttonSingUp.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    //método para fazer o cadastro do usuário e verificar se os campos estão preenchidos
+    private fun registerUser() {
+        val userName: String = userNameEditText.text.toString()
+        val email: String = emailEditText.text.toString()
+        val password: String = passwordEditText.text.toString()
+
+        if (email.isEmpty() || userName.isEmpty() || password.isEmpty()) {
+            toast("Preencha todos os campos!!!")
+        } else {
+            //Cria o usuário com o email ou senha
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+
+                    if (it.isSuccessful) {
+                        toast("Cadastro realizado com SUCESSO!!!")
+                    }
+                }.addOnFailureListener /*caso n cadastre o usuário gera uma toast de erro*/{
+                    toast("Erro ao cadastrar o usuário x(")
+                }
+        }
     }
 }
